87f8e3a19bc0c47191133da77987d7802ff124f1
David Colls <dcolls@dcmbp.local>
2020-04-05 07:45:52 +1000

    improve readability

--- src/openspacial.py
+++ src/openspacial.py
@@ -6,17 +6,19 @@ from solver import Solver
 def main():
     trials = 10000
     print(f"Number of samples for each p: {trials}")
-
+    # occupancy proportions to test
     ps = [x / 10.0 for x in range(11)]
     ps.reverse()
 
     for p in ps:
         s = 0
         for i in range(trials):
+            # set up office and scenario
             og = OfficeGraph()
-            solver = Solver(og)
             occupancy = Occupancy(og.back_row(), og.all_desks(), p)
             og.apply_occupancy(occupancy.seating)
+            # solve and tally result
+            solver = Solver(og)
             s = s + solver.can_haz_food(occupancy.source)
         rate = s / trials
         print("{:.1f} {:.3f}".format(p, rate))
