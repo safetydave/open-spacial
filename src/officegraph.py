1f60ca3d08063114157463dc176e44c393f61312
David Colls <dcolls@dcmbp.local>
2020-04-04 13:46:07 +1100

    add nodes to office graph

--- src/officegraph.py
+++ src/officegraph.py
@@ -1,9 +1,16 @@
+import networkx as nx
+
+
 class OfficeGraph:
 
-    rank = 10
-
-    def __init__(self, rank):
+    def __init__(self, rank=10):
         self.rank = rank
+        self.target = rank ** 2
+        self.graph = nx.Graph()
 
-    def target(self):
-        return self.rank ** 2
+        self.graph.add_node(self.target)
+
+        for i in range(rank):
+            for j in range(rank):
+                n = i * rank + j
+                self.graph.add_node(n)
--- test/testofficegraph.py
+++ test/testofficegraph.py
@@ -9,8 +9,17 @@ from src.officegraph import OfficeGraph
 class TestOfficeGraph(unittest.TestCase):
 
     def test_target(self):
+        og = OfficeGraph(10)
+        self.assertEqual(100, og.target)
         og = OfficeGraph(5)
-        self.assertEqual(og.target(), 25)
+        self.assertEqual(25, og.target)
+
+    # Grid of 100 desks and 1 food truck
+    def test_num_nodes(self):
+        og = OfficeGraph(10)
+        self.assertEqual(101, len(og.graph.nodes))
+        og = OfficeGraph(5)
+        self.assertEqual(26, len(og.graph.nodes))
 
 
 if __name__ == '__main__':
