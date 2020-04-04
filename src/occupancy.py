ab6e49626a3a8b9e887ff8151996fa795f4ca7c3
David Colls <dcolls@dcmbp.local>
2020-04-04 16:21:52 +1100

    add seating to occupancy

--- src/occupancy.py
+++ src/occupancy.py
@@ -3,12 +3,20 @@ import random
 
 class Occupancy:
 
-    def __init__(self, og, seed=None):
+    def __init__(self, og, p, seed=None):
         self.og = og
+        self.p = p
 
         if seed:
             random.seed(seed)
         self.source = self.generate_source()
+        self.seating = self.generate_seating()
 
     def generate_source(self):
         return random.choice(range(self.og.rank))
+
+    def generate_seating(self):
+        excluded_nodes = set([self.source, self.og.target])
+        open_seats = set(self.og.graph.nodes).difference(excluded_nodes)
+        num_seats = int(round(self.p * len(open_seats)))
+        return random.sample(open_seats, num_seats)
--- test/testoccupancy.py
+++ test/testoccupancy.py
@@ -11,10 +11,27 @@ class TestOccupancy(unittest.TestCase):
 
     def setUp(self):
         self.og10 = OfficeGraph(10)
-        self.occ_rnd = Occupancy(self.og10)
-        self.occ_42 = Occupancy(self.og10, seed=42)
+        #todo this could be a property of OfficeGraph
+        self.all_seats = set(self.og10.graph.nodes).difference(set([self.og10.target]))
+        self.occ_rnd_min = Occupancy(self.og10, 0.0)
+        self.occ_rnd_mid = Occupancy(self.og10, 0.5)
+        self.occ_rnd_max = Occupancy(self.og10, 1.0)
+        self.occ_42 = Occupancy(self.og10, 0.5, seed=42)
 
+    # source is always in the first row
     def test_source(self):
-        self.assertGreaterEqual(self.occ_rnd.source, 0)
-        self.assertLessEqual(self.occ_rnd.source, 9)
+        self.assertGreaterEqual(self.occ_rnd_min.source, 0)
+        self.assertLessEqual(self.occ_rnd_min.source, 9)
         self.assertEqual(self.occ_42.source, 1)
+
+    # p = 0.0 is empty office except you
+    # p = 1.0 is all office full including you
+    def test_seating(self):
+        self.assertEqual(0, len(self.occ_rnd_min.seating))
+        self.assertTrue(set(self.occ_rnd_min.seating).issubset(self.all_seats))
+        self.assertEqual(50, len(self.occ_rnd_mid.seating))
+        self.assertTrue(set(self.occ_rnd_mid.seating).issubset(self.all_seats))
+        self.assertEqual(99, len(self.occ_rnd_max.seating))
+        self.assertTrue(set(self.occ_rnd_max.seating).issubset(self.all_seats))
+
+    #todo test distributions
