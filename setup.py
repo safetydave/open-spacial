d1ba2248c37adcd4905c467c3d3e6d27ccb26287
David Colls <dcolls@dcmbp.local>
2020-05-10 21:32:17 +1000

    fix path references for package

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='ospacial',
       version='0.1',
@@ -7,7 +7,8 @@ setup(name='ospacial',
       author='David Colls',
       author_email='david dot colls at gmail com',
       license='MIT',
-      packages=['ospacial'],
       python_requires='>=3.6.0',
+      packages=find_packages("src"),
+      package_dir={"": "src"},
       install_requires=['matplotlib', 'networkx', 'numpy', 'pandas', 'scipy'],
       zip_safe=False)
